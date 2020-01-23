c481d9576f6d4dc082fd3c955073cf3a3974ac5b
bak83 <bak.dariusz@gmail.com>
2020-01-23 20:02:55 +0100

    początek

--- zad8_Vector/Program.cs
+++ zad8_Vector/Program.cs
@@ -6,7 +6,13 @@ namespace zad8_Vector
 	{
 		static void Main(string[] args)
 		{
+			var v1 = new Vector(5);
+			var v2 = new Vector(5);
+			v1.fillMatrix();
+			v2.fillMatrix();
 			Console.WriteLine("Hello World!");
+			Console.WriteLine(v2.V.Length);
+			Console.WriteLine(v2.V[0]);
 		}
 	}
 }
---
+++ zad8_Vector/Vector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace zad8_Vector
+{
+	class Vector
+	{
+		public double[] V;
+		
+		
+		public void fillMatrix()
+		{
+			for (int i = 0; i < V.Length; i++)
+			{
+				Console.WriteLine("Enter number: " + i+1);
+				V[i] = int.Parse(Console.ReadLine());
+			}
+
+		}
+		public Vector(int v)
+		{
+			V = new double[v];
+		}
+
+		public double add(double[] v1,double []v2)
+		{
+			return 0;
+		}
+	}
+}
